6306bad4a44cfcf1375ee8f613d3a23b2eea6a5a
matheuz233 <matheusaugus233@gmail.com>
2025-04-07 10:00:09 -0300

    feat: :rocket: Algoritmo de Teste para Primo

--- Algoritmos/Outros/primo.py
+++ Algoritmos/Outros/primo.py
@@ -1,8 +1,8 @@
 def is_prime(number):
     if number <= 1:
-        return 0
+        return False
 
     for x in range(2, number):
         if not number % x:
-            return 0
-    return 1
+            return False
+    return True
